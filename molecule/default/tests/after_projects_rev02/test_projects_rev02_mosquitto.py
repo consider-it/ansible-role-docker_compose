61e23bf1d17735b4eb6215a1c561141669a68d20
Julian Schacher <schacher@consider-it.de>
2024-02-28 14:13:36 +0100

    test: adjust permissions check to work within gits limitations
    Since git apparently doesn't track directory permissions at all and only
    tracks whether or not a file is marked as executable, mark the
    compose.yaml file as executable and check for that in the verify step.

--- molecule/default/tests/after_projects_rev02/test_projects_rev02_mosquitto.py
+++ molecule/default/tests/after_projects_rev02/test_projects_rev02_mosquitto.py
@@ -3,10 +3,10 @@ def test_files(host):
     assert mosquitto_dir.is_directory == True
     assert mosquitto_dir.user == "root"
     assert mosquitto_dir.group == "root"
-    assert mosquitto_dir.mode == 0o700
+    assert mosquitto_dir.mode == 0o755
 
     compose_file = host.file("/ansible_docker_compose/projects/mosquitto/compose.yaml")
     assert compose_file.is_file == True
     assert compose_file.user == "root"
     assert compose_file.group == "root"
-    assert compose_file.mode == 0o600
+    assert compose_file.mode == 0o755
